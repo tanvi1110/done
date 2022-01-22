0ed1fccda279797b6b3860800cc1c7740ddb39cd
tanvi <tanvi221110@gmail.com>
2022-01-22 04:34:10 -0800

    INITIALISED

---
+++ ndfnf.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+ 
+ int main()
+ {  
+     int n ;
+      cout<<"Enter the number";
+      cin>>n;
+   if((n%5==0) || (n%11==0))
+		{
+		System.out.println("ONE");
+		}
+		else if((n%5==0) && (n%11==0))
+		{
+		    System.out.println("BOTH");
+		}
+		else
+		{
+		System.out.println("NONE");
+		}
+	
+
+
+
+ }
